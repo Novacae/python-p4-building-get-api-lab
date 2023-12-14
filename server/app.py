eb539879c5acb6be6fdfe4e51a6419439e389b8e
aman githinji <amangithinji09@gmail.com>
2023-12-14 20:25:31 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -2,12 +2,67 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
-from sqlalchemy.orm import sessionmaker
-from models import db, Bakery, BakedGood
-from sqlalchemy import create_engine
+from sqlalchemy import desc
 
+from models import db, Bakery, BakedGood
+from sqlalchemy_serializer import SerializerMixin
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return '<h1>Bakery GET API</h1>'
+
+@app.route('/bakeries')
+def bakeries():
+    bakeries = Bakery.query.all()
+    response = make_response(
+        jsonify([bakery.to_dict() for bakery in bakeries]),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+@app.route('/bakeries/<int:id>')
+def bakery_by_id(id):
+    bakery = Bakery.query.get(id)
+    response = make_response(
+        jsonify(bakery.to_dict(include=['baked_goods'])),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+@app.route('/baked_goods/by_price')
+def baked_goods_by_price():
+    goods = BakedGood.query.order_by(desc(BakedGood.price)).all()
+    response = make_response(
+        jsonify([good.to_dict() for good in goods]),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+@app.route('/baked_goods/most_expensive')
+def most_expensive_baked_good():
+    most_expensive_good = BakedGood.query.order_by(desc(BakedGood.price)).first()
+    response = make_response(
+        jsonify(most_expensive_good.to_dict()),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+
+"""app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -62,4 +117,4 @@ def get_most_expensive_baked_good():
     return jsonify(baked_good_json)
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555, debug=True)"""
